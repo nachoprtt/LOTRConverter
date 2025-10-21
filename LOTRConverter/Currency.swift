4ab01930ee63ae9299188c3d34bdaa6b12be57a5
Nacho <ignacio.s.pratto@gmail.com>
2025-10-21 17:46:56 -0300

    lesson 11 finished

---
+++ LOTRConverter/Currency.swift
@@ -0,0 +1,14 @@
+//
+//  Currency.swift
+//  LOTRConverter
+//
+//  Created by Pratto on 21/10/2025.
+//
+
+enum Currency: Double {
+    case copperPenny = 6400
+    case silverPenny = 64
+    case silverPiece = 16
+    case goldenPenny = 4
+    case goldenPiece = 1
+}
