e13a15c5dc274ccb8de851b5dec2d2c8ccd7c5fc
Nacho <ignacio.s.pratto@gmail.com>
2025-10-22 15:13:17 -0300

    lesson 12 finished

--- LOTRConverter/Currency.swift
+++ LOTRConverter/Currency.swift
@@ -5,10 +5,42 @@
 //  Created by Pratto on 21/10/2025.
 //
 
+import SwiftUI
+
 enum Currency: Double {
     case copperPenny = 6400
     case silverPenny = 64
     case silverPiece = 16
-    case goldenPenny = 4
-    case goldenPiece = 1
+    case goldPenny = 4
+    case goldPiece = 1
+    
+    var image: ImageResource {
+        switch self {
+        case .copperPenny:
+                .copperpenny
+        case .silverPenny:
+                .silverpenny
+        case .silverPiece:
+                .silverpiece
+        case .goldPenny:
+                .goldpenny
+        case .goldPiece:
+                .goldpiece
+        }
+    }
+    
+    var name: String{
+        switch self {
+        case .copperPenny:
+            "Copper Penny"
+        case .silverPenny:
+            "Silver Penny"
+        case .silverPiece:
+            "Silver Piece"
+        case .goldPenny:
+            "Gold Penny"
+        case .goldPiece:
+            "Gold Piece"
+        }
+    }
 }
